798a69bfa658af4aae7932933f2d55336ac8a833
kvs23031999 <kvs23031999@gmail.com>
2023-10-11 12:47:07 +0300

    add new system out println

--- src/HWGit4.java
+++ src/HWGit4.java
@@ -3,5 +3,6 @@ public class HWGit4 {
     public static void main(String[] args){
 
         System.out.println("Kulishov" + " Java Basic" + " 11.10.2023");
+        System.out.println("Hello again");
     }
 }
